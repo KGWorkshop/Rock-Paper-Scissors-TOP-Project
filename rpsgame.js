62040813dba461a9079e331f896e952e9a6e59b1
KGWorkshop <computejunkie@gmail.com>
2025-10-15 23:10:03 -0400

    Implemented the playGame Function
    Implementing the playGame function so that multiple rounds can be played
    between the user and computer. Nested the playRound function into the
    playGame function so that it had access to the playRound variables. Also
    moved the humanScore and computerScore from a global variable to a local
    one inside of playGame. Then I called the playGame function to test
    functionality.

--- rpsgame.js
+++ rpsgame.js
@@ -1,47 +1,57 @@
-let humanScore = 0;
-let computerScore = 0;
-
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
 function getComputerChoice() {
-	const rand = Math.floor(Math.random() * 3);
-	switch (rand) {
-		case 0: return 'rock'
-		case 1: return 'paper'
-		case 2: return 'scissors'
-	}
+  const rand = Math.floor(Math.random() * 3);
+  switch (rand) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+  }
 }
 
 function getHumanChoice() {
-	let usrChoice = prompt("Rock, Paper or Scissors?");
-	return usrChoice.toLowerCase();
+  let usrChoice = prompt("Rock, Paper or Scissors?");
+  return usrChoice.toLowerCase();
 }
 
-function playRound(humanChoice, computerChoice) {
-	if (humanChoice === "paper" && computerChoice === "rock") {
-		console.log("You win! Paper beats Rock.");
-		humanScore += 1;
-	} else if (humanChoice === "rock" && computerChoice === "paper") {
-		console.log("You lose! Paper beats Rock.");
-		computerScore += 1;
-	} else if (humanChoice === "scissors" && computerChoice === "paper") {
-		console.log("You win! Scissors beat Paper.");
-		humanScore += 1;
-	} else if (humanChoice === "paper" && computerChoice === "scissors") {
-		console.log("You lose! Scissors beat Paper.");
-		computerScore += 1;
-	} else if (humanChoice === "rock" && computerChoice === "scissors") {
-		console.log("You win! Rock beat Scissors.");
-		humanScore += 1;
-	} else if (humanChoice === "scissors" && computerChoice === "rock") {
-		console.log("You lose! Rock beat Scissors.");
-		computerScore += 1;
-	} else {
-		console.log("Draw.");
-	}
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "paper" && computerChoice === "rock") {
+      console.log("You win! Paper beats Rock.");
+      humanScore += 1;
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+      console.log("You lose! Paper beats Rock.");
+      computerScore += 1;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+      console.log("You win! Scissors beat Paper.");
+      humanScore += 1;
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+      console.log("You lose! Scissors beat Paper.");
+      computerScore += 1;
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+      console.log("You win! Rock beat Scissors.");
+      humanScore += 1;
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+      console.log("You lose! Rock beat Scissors.");
+      computerScore += 1;
+    } else {
+      console.log("Draw.");
+    }
+  }
+  playRound(humanSelection, computerSelection);
+  playRound(humanSelection, computerSelection);
+  playRound(humanSelection, computerSelection);
+  playRound(humanSelection, computerSelection);
+  playRound(humanSelection, computerSelection);
 }
 
-playRound(humanSelection, computerSelection);
+playGame();
 console.log(humanScore);
 console.log(computerScore);
