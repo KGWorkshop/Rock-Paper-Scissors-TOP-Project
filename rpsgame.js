49afdc1f165291463e0ae8504de94b964a7e68aa
KGWorkshop <computejunkie@gmail.com>
2025-10-15 22:10:56 -0400

    Implement playRound Functionality
    Implemented the playRound function to simulate rounds being played by
    the computer and a user. The function takes two args humanChoice and
    computerChoice. I then create an if block for all scenarios of the game
    and also increment global variables humanScore and computerScore by 1
    depending on who won that round. Then allocate the other global variables
    humanSelection and computerSelection to previous functions
    getHumanChoice and getComputerChoice respectively. Then I call the
    playRound function to test it.

--- rpsgame.js
+++ rpsgame.js
@@ -1,3 +1,9 @@
+let humanScore = 0;
+let computerScore = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
 function getComputerChoice() {
 	const rand = Math.floor(Math.random() * 3);
 	switch (rand) {
@@ -12,5 +18,30 @@ function getHumanChoice() {
 	return usrChoice;
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+	if (humanChoice === "paper" && computerChoice === "rock") {
+		console.log("You win! Paper beats Rock.");
+		humanScore += 1;
+	} else if (humanChoice === "rock" && computerChoice === "paper") {
+		console.log("You lose! Paper beats Rock.");
+		computerScore += 1;
+	} else if (humanChoice === "scissors" && computerChoice === "paper") {
+		console.log("You win! Scissors beat Paper.");
+		humanScore += 1;
+	} else if (humanChoice === "paper" && computerChoice === "scissors") {
+		console.log("You lose! Scissors beat Paper.");
+		computerScore += 1;
+	} else if (humanChoice === "rock" && computerChoice === "scissors") {
+		console.log("You win! Rock beat Scissors.");
+		humanScore += 1;
+	} else if (humanChoice === "scissors" && computerChoice === "rock") {
+		console.log("You lose! Rock beat Scissors.");
+		computerScore += 1;
+	} else {
+		console.log("Draw.");
+	}
+}
+
+playRound(humanSelection, computerSelection);
+console.log(humanScore);
+console.log(computerScore);
